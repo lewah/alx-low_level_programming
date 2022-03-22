9c11e7f6ba529e9802914d918f39d1455791cb0b
lewah <ingridlewah@gmail.com>
2022-03-23 02:51:50 +0300

    unction that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function
+ * @s: parameter passed
+ * Return: integer
+*/
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
